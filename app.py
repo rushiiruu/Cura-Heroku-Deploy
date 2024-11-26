8d0d43da0b9999e833642d6b8bb8ab8cf3b366da
Rusyl <116929509+rushiiruu@users.noreply.github.com>
2024-11-26 18:18:32 +0800

    Update app.py

--- app.py
+++ app.py
@@ -77,11 +77,8 @@ def process_image():
         
         return jsonify(response), 200
 
-
-
     except Exception as e:
-        return jsonify({'error': str(e), 'type': str(type(e))}), 500    
-        
+        return jsonify({'error': str(e), 'type': str(type(e))}), 500
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
